b973c2e70ab032184993db28f6d421fb082219f5
VitorAlvess <53319601+VitorAlvess@users.noreply.github.com>
2022-11-24 20:38:12 -0300

    Etapa 1: Login no Atados e pagina do PiPA

--- main.py
+++ main.py
@@ -6,51 +6,73 @@ import json
 
 class automacao: 
     def __init__(self):
-        servico = Service(ChromeDriverManager().install())
-        self.navegar = webdriver.Chrome(service=servico)
-    def iniciar(self):
-        self.ler_dados_json()
-        self.atados()
-        
-
-    def ler_dados_json(self):
-         with open("settings_pessoal.json", encoding='utf-8') as meu_json: # Importar dados de um arquivo config com usernames e passwords
+        with open("settings_pessoal.json", encoding='utf-8') as meu_json: # Importar dados de um arquivo config com usernames e passwords
             dados = json.load(meu_json)
             self.email = dados["email"]
             self.senha = dados["password"]
             self.acesso = dados["access"]
+        servico = Service(ChromeDriverManager().install())
+        self.navegar = webdriver.Chrome(service=servico)
+        
+    def iniciar(self):
+        self.atados()    
+
 
     def atados(self):
         pagina = self.navegar
 
         pagina.get('https://atados.com.br')
-        pagina.maximize_window()
+        pagina.maximize_window() #provavelmente tirar isso depois para passar para o servidor
         pagina.find_element('xpath',' //*[@id="toolbar-auth-button"]' ).click()
         if self.acesso == 'mail':
             pagina.find_element('xpath',' /html/body/reach-portal/div[3]/div/div/div/div/div[1]/div/button[1]' ).click()
             pagina.find_element('xpath',' /html/body/reach-portal/div[3]/div/div/div/form/div/div[2]/input' ).send_keys(self.email)
             pagina.find_element('xpath',' /html/body/reach-portal/div[3]/div/div/div/form/div/div[3]/input' ).send_keys(self.senha)
             pagina.find_element('xpath',' /html/body/reach-portal/div[3]/div/div/div/form/div/button' ).click()
-        elif self.acesso == 'google':
+        elif self.acesso == 'google':  
             pagina.find_element('xpath','/html/body/reach-portal/div[3]/div/div/div/div/div[1]/div/button[3]' ).click()
             sleep(5)
             print(pagina.current_window_handle)
             print(pagina.window_handles)
-   
             sleep(5)
 
-            pagina.find_element('xpath','//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]').click()
-            pagina.find_element('xpath','//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]').send_keys(self.email)
-
-
+            wids = pagina.window_handles
+            for window in wids:
+                pagina.switch_to.window(window)
+                if 'google' in pagina.current_url:
+                    print(pagina.current_url)
+                    print("achei")
+                    break
+                    
+            pagina.find_element('xpath','//*[@id="identifierId"]' ).send_keys(self.email)
+            pagina.find_element('xpath',' //*[@id="identifierNext"]' ).click()
+            print('cliquei em next')
+            sleep(10)
+            pagina.find_element('xpath','//*[@id="password"]/div[1]/div/div[1]/input' ).send_keys(self.senha)
+            pagina.find_element('xpath','//*[@id="passwordNext"]' ).click()
+            sleep(10)
+            for window in wids:
+                pagina.switch_to.window(window)
+                if 'atados' in pagina.current_url:
+                    print(pagina.current_url)
+                    break
         else:
             print('Por favor Insira no arquivo settings.json uma configuração de access de "mail" ou "google" ')
+        pagina.get('https://www.atados.com.br/ong/pipa')
+        print('ue')
+    
 
 
 
 
 
-        sleep(100)
+
+
+
+
+
+
+        sleep(10)
     
 
 start = automacao()
